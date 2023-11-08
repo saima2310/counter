fb747f34f5da12e3087d285eade234d7aff7f0d1
Saima Zaki <105597579+saima2310@users.noreply.github.com>
2023-11-08 14:40:55 +0530

    Update Template.js

--- packages/ui-library/src/Template.js
+++ packages/ui-library/src/Template.js
@@ -2,13 +2,13 @@
 
 import { h } from 'snabbdom';
 
-// `createView` is a factory function for generating virtual DOM templates
+
 export const createView = (state, props, hooks) => {
-  // Define your template using Snabbdom's `h` function
+  
   return h('div#container', [
-    h('h1', { hook: hooks }, state.title), // `state.title` is dynamically rendered
+    h('h1', { hook: hooks }, state.title), 
     h('button', {
-      on: { click: props.handleClick },    // Event listeners are directly attached
+      on: { click: props.handleClick },  
     }, 'Add')
   ]);
 };
